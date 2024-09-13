2ad805e738323f162add78c081c92e0851d9fe13
= <=>
2024-09-13 19:49:54 +0530

    final push

--- Auth/app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const app = e()
-
-
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.user
--- Auth/router/routs.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-
-const rout =  Router()
-
-rout.route("/",)
